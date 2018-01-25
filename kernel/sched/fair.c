60e4203afb44d062b6b3c563094c360c9ceaa27d
Youngtae Lee <yt0729.lee@samsung.com>
2018-01-25 14:40:18 +0900

    samsung: emc: introduce exynos mode changer
    This driver recomands optimized mode for performance
    in Exynos SoC supporting boosting
    
    Change-Id: Ie1592af0a961fcf3f96c845c26b9bd440fb88b92
    Signed-off-by: Youngtae Lee <yt0729.lee@samsung.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -36,6 +36,8 @@
 
 #include <trace/events/sched.h>
 
+#include <soc/samsung/exynos-emc.h>
+
 #include "sched.h"
 #include "tune.h"
 #include "walt.h"
@@ -11118,6 +11120,9 @@ void nohz_balance_enter_idle(int cpu)
 	if (on_null_domain(cpu_rq(cpu)))
 		return;
 
+#ifdef CONFIG_EXYNOS_MODE_CHANGER
+	exynos_emc_update(cpu);
+#endif
 	cpumask_set_cpu(cpu, nohz.idle_cpus_mask);
 	atomic_inc(&nohz.nr_cpus);
 	set_bit(NOHZ_TICK_STOPPED, nohz_flags(cpu));
@@ -11523,6 +11528,10 @@ static void task_tick_fair(struct rq *rq, struct task_struct *curr, int queued)
 	update_misfit_status(curr, rq);
 
 	update_overutilized_status(rq);
+
+#ifdef CONFIG_EXYNOS_MODE_CHANGER
+	exynos_emc_update(rq->cpu);
+#endif
 }
 
 /*
