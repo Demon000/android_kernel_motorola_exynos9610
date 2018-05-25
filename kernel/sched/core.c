754dee65ab23b566caa031cd973a4ae261a211bc
Park Bumgyu <bumgyu.park@samsung.com>
2018-05-25 14:01:52 +0900

    sched: ems: introduce task band
    Change-Id: Ic3fbe3e80c8033f5c1c77f02cb0eeb6ee04d9630
    Signed-off-by: Park Bumgyu <bumgyu.park@samsung.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -29,6 +29,7 @@
 #include <linux/security.h>
 #include <linux/syscalls.h>
 #include <linux/debug-snapshot.h>
+#include <linux/ems.h>
 
 #include <asm/switch_to.h>
 #include <asm/tlb.h>
@@ -2245,6 +2246,10 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 #endif
 
 	INIT_LIST_HEAD(&p->se.group_node);
+#ifdef CONFIG_SCHED_EMS
+	rcu_assign_pointer(p->band, NULL);
+	INIT_LIST_HEAD(&p->band_members);
+#endif
 	walt_init_new_task_load(p);
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
@@ -2523,6 +2528,8 @@ void wake_up_new_task(struct task_struct *p)
 	struct rq *rq;
 
 	raw_spin_lock_irqsave(&p->pi_lock, rf.flags);
+
+	newbie_join_band(p);
 
 	walt_init_new_task_load(p);
 
@@ -3118,6 +3125,8 @@ void scheduler_tick(void)
 	trigger_load_balance(rq);
 #endif
 	rq_last_tick_reset(rq);
+
+	update_band(curr, -1);
 }
 
 #ifdef CONFIG_NO_HZ_FULL
@@ -6026,6 +6035,8 @@ void __init sched_init(void)
 	}
 
 	set_load_weight(&init_task);
+
+	alloc_bands();
 
 	/*
 	 * The boot idle thread does lazy MMU switching as well:
