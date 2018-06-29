bd8ab80f7c5d42e68cafd419476a5f7a54d6e62c
hyesoo.yu <hyesoo.yu@samsung.com>
2018-06-29 09:56:53 +0900

    dma-buf: introduce dma-buf trace
    We introduce the concept to get information about
    what process has allocated and used the dmabuf.
    
    The reference is registered and released by the user.
    This information is supported by debugfs with pss.
    
    The temporary reference is always registered
    by allocation to indicate the user to request allocation,
    and this reference is deleted when user requests
    a reference explicitly.
    
    The reference ensure to be released when the buffer
    is removed or the user process of handle is exited.
    
    Change-Id: I4c147ad91058cf04b81f51753cbefcc420ec8389
    Signed-off-by: hyesoo.yu <hyesoo.yu@samsung.com>

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -40,6 +40,7 @@
 #include <uapi/linux/magic.h>
 
 #include "dma-buf-container.h"
+#include "dma-buf-trace.h"
 
 static inline int is_dma_buf_file(struct file *);
 
@@ -85,6 +86,8 @@ static void dma_buf_release(struct dentry *dentry)
 	BUG_ON(dmabuf->cb_shared.active || dmabuf->cb_excl.active);
 
 	dmabuf->ops->release(dmabuf);
+
+	dmabuf_trace_free(dmabuf);
 
 	if (dmabuf->resv == (struct reservation_object *)&dmabuf[1])
 		reservation_object_fini(dmabuf->resv);
@@ -414,6 +417,10 @@ static long dma_buf_ioctl(struct file *file,
 		return dmabuf_container_set_mask_user(dmabuf, arg);
 	case DMA_BUF_IOCTL_CONTAINER_GET_MASK:
 		return dmabuf_container_get_mask_user(dmabuf, arg);
+	case DMA_BUF_IOCTL_TRACK:
+		return dmabuf_trace_track_buffer(dmabuf);
+	case DMA_BUF_IOCTL_UNTRACK:
+		return dmabuf_trace_untrack_buffer(dmabuf);
 	default:
 		return -ENOTTY;
 	}
@@ -603,6 +610,8 @@ struct dma_buf *dma_buf_export(const struct dma_buf_export_info *exp_info)
 	mutex_lock(&db_list.lock);
 	list_add(&dmabuf->list_node, &db_list.head);
 	mutex_unlock(&db_list.lock);
+
+	dmabuf_trace_alloc(dmabuf);
 
 	return dmabuf;
 
@@ -1307,7 +1316,7 @@ static const struct file_operations dma_buf_debug_fops = {
 	.release        = single_release,
 };
 
-static struct dentry *dma_buf_debugfs_dir;
+struct dentry *dma_buf_debugfs_dir;
 
 static int dma_buf_init_debugfs(void)
 {
