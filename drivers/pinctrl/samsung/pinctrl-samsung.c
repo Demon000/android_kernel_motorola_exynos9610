samsungexynos9810/kernel_google_exynos9110

[COMMON] pinctrl: samsung: Fix pm function
gpz's power is controlled by audio block. This patch does not execute
pm function when its ctrl has no suspend or resume function.

This commit is based on ab55ec9f9b6e0a053052c8c1a40da6a84b8b6600.

Change-Id: I05accfa7610b5f0cce4e742c2c9d977cbbd07e06
Signed-off-by: eunki_kim <eunki_kim@samsung.com>

drivers/pinctrl/samsung/pinctrl-samsung.c
@@ -1191,6 +1191,9 @@ static void samsung_pinctrl_suspend_dev(
 	void __iomem *virt_base = drvdata->virt_base;
 	int i;
 
+	if (!ctrl->suspend)
+		return;
+
 	for (i = 0; i < ctrl->nr_banks; i++) {
 		struct samsung_pin_bank *bank = &ctrl->pin_banks[i];
 		void __iomem *reg = virt_base + bank->pctl_offset;
@@ -1221,8 +1224,7 @@ static void samsung_pinctrl_suspend_dev(
 		}
 	}
 
-	if (ctrl->suspend)
-		ctrl->suspend(drvdata);
+	ctrl->suspend(drvdata);
 }
 
 /**
@@ -1239,8 +1241,10 @@ static void samsung_pinctrl_resume_dev(struct samsung_pinctrl_drv_data *drvdata)
 	void __iomem *virt_base = drvdata->virt_base;
 	int i;
 
-	if (ctrl->resume)
-		ctrl->resume(drvdata);
+	if (!ctrl->resume)
+		return;
+
+	ctrl->resume(drvdata);
 
 	for (i = 0; i < ctrl->nr_banks; i++) {
 		struct samsung_pin_bank *bank = &ctrl->pin_banks[i];

Demon000/android_kernel_motorola_exynos9610

Signed-off-by: Gyeongtaek Lee <gt82.lee@samsung.com>

drivers/pinctrl/samsung/pinctrl-samsung.c
@@ -1275,6 +1275,9 @@ static void samsung_pinctrl_suspend_dev(
 	if (ret)
 	        dev_err(drvdata->dev, "could not set sleep pinstate %d\n", ret);
 
+	if (!drvdata->suspend)
+		return;
+
 	for (i = 0; i < drvdata->nr_banks; i++) {
 		struct samsung_pin_bank *bank = &drvdata->pin_banks[i];
 		void __iomem *reg = bank->pctl_base + bank->pctl_offset;
@@ -1304,8 +1307,7 @@ static void samsung_pinctrl_suspend_dev(
 		}
 	}
 
-	if (drvdata->suspend)
-		drvdata->suspend(drvdata);
+	drvdata->suspend(drvdata);
 	if (drvdata->retention_ctrl && drvdata->retention_ctrl->enable)
 		drvdata->retention_ctrl->enable(drvdata);
 }
@@ -1322,8 +1324,10 @@ static void samsung_pinctrl_resume_dev(struct samsung_pinctrl_drv_data *drvdata)
 {
 	int i;
 
-	if (drvdata->resume)
-		drvdata->resume(drvdata);
+	if (!drvdata->resume)
+		return;
+
+	drvdata->resume(drvdata);
 
 	for (i = 0; i < drvdata->nr_banks; i++) {
 		struct samsung_pin_bank *bank = &drvdata->pin_banks[i];